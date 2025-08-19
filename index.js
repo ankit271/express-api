70c68572c8f38b4c3dc8a3cfff67e97d0da87fea
Ankit Mishra <atmishra243@gmail.com>
2025-08-19 09:53:19 +0530

    User Management

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const PORT = 4000
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
@@ -20,11 +24,15 @@ app.get('/', (req, res) => {
     res.send('Welcome to the User Management');
 })
 
+app.get("/create", (req, res) => {
+  res.render("create", { message: null });
+});
+
 app.get('/users', (req, res) =>{  
-  res.render('index', {title : "User List", users : users} );
+  res.render('index', {title : "User List", users : users, message: null} );
 })
 
-app.post('/user', (req, res) =>{
+app.post('/create', (req, res) =>{
 try {
   
     const { name, email, city } = req.body;
@@ -32,24 +40,24 @@ try {
 
     users.push({id, name, email, city});
     
-    res.status(201).json({ "message": "user created successfully"})
-
+    res.status(201).render("create", { message: "user created successfully" });    
 } catch (error) {
     res.status(500).json({message: "Internal Server Error"});
   }
 })
 
+
 app.get('/user/:id', (req, res) =>{
   const userId = req.params.id
   const user = users.find(user => user.id == userId)
   
-  res.status(200).json({"user": user});
+  res.status(200).render('edit', {title : "Edit User", user : user, message: null} );  
 })
 
-app.put('/user/:id', (req, res) =>{
+app.post('/update/:id', (req, res) =>{
   try {
       
-    const userId = req.params.id
+      const userId = req.params.id
       const { name, email, city } = req.body;
       const userIndex = users.findIndex(user => user.id == userId)
       
@@ -60,14 +68,14 @@ app.put('/user/:id', (req, res) =>{
       if(userIndex != -1){
         users[userIndex] = {id: userId, name, email, city}  
       }
-      res.status(200).json({"message": "  User updated successfully", "user": users[userIndex]});
+      res.status(200).render('index', {title : "User List" ,message: "User updated successfully", users: users});
 
   } catch (error) {
      res.status(500).json({message: "Internal Server Error"});
   }
 })
 
-app.delete('/user/:id', (req, res) =>{
+app.post('/delete/:id', (req, res) =>{
   try {
       const userId = req.params.id
       const userIndex = users.findIndex(user => user.id == userId)
@@ -78,7 +86,8 @@ app.delete('/user/:id', (req, res) =>{
 
       if(userIndex != -1){
         users.splice(userIndex, 1) 
-        res.status(200).json({"message": "  User deleted successfully"});
+        res.status(200).render('index', {title : "User List", users : users, message: " User deleted successfully"} );
+        //res.status(200).json({"message": "  User deleted successfully"});
       }
   } catch (error) {
     res.status(500).json({message: "Internal Server Error"});
